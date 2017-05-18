4f6141dea29fb656e082b4c9d8b6872d47eea184
fengzhuojia <fengzhuojia@appledeMacBook-Air.local>
2017-05-18 19:34:31 +0800

    preset es2015 stage02

--- app/javascript/vue-admin/routes.js
+++ app/javascript/vue-admin/routes.js
@@ -5,6 +5,7 @@ import Main from './views/Main.vue'
 import Table from './views/nav1/Table.vue'
 import Form from './views/nav1/Form.vue'
 import user from './views/nav1/user.vue'
+import Page4 from './views/nav2/Page4.vue'
 import Page5 from './views/nav2/Page5.vue'
 import Page6 from './views/nav3/Page6.vue'
 import echarts from './views/charts/echarts.vue'
@@ -41,6 +42,7 @@ let routes = [
         name: '导航二',
         iconCls: 'fa fa-id-card-o',
         children: [
+            { path: '/page4', component: Page4, name: '页面4' },
             { path: '/page5', component: Page5, name: '页面5' }
         ]
     },
