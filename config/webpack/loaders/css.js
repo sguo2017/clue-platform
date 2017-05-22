990842df7d583172d16944fb0b050071e1904059
fengzhuojia <fengzhuojia@appledeMacBook-Air.local>
2017-05-22 22:00:40 +0800

    loaders

--- config/webpack/loaders/css.js
+++ config/webpack/loaders/css.js
@@ -2,13 +2,12 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const { env } = require('../configuration.js')
 
 module.exports = {
-  test: /\.(scss|sass)$/i,
+  test: /\.css$/i,
   use: ExtractTextPlugin.extract({
     fallback: 'style-loader',
     use: [
       { loader: 'css-loader', options: { minimize: env.NODE_ENV === 'production' } },
-      'postcss-loader',
-      'sass-loader'
+      'postcss-loader'
     ]
   })
 }
