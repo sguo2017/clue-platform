4d94ebf6adf6c53d894b567dd5b6a162aab7045b
witschia <witschia@163.com>
2017-06-02 16:28:38 +0800

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-  before_action :authenticate_user!,except: [:import]
-=======
   before_action :authenticate_user!
->>>>>>> cc38c56cee565ae575ec92a60c5f09698174fcef
 end
