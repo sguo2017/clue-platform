0e52e16cc09f2bf95d7da6af78ae8f95a3155101
Guirong Zeng <1358073296@.com>
2017-07-18 16:49:10 +0800

    加入造数据功能(rails db:seed ,生产管理员和calllist)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Calllist.all.size<1000
+  $start=18620788695
+  while $start<=18620789695 do
+    for j in 1..100
+      Calllist.create(:from_num=>$start,:to_num=>$start+j)
+    end
+    $start=$start+101
+  end
+end
+if User.where(:email=>'admin@qq.com').size==0
+  User.create(:email=>"admin@qq.com",:password=>'888888')
+end
