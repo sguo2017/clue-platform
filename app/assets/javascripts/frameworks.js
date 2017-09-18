fe44e947f4d18ce13eebad3eb9119db28beac218
rong <1358073296@qq.com>
2017-09-18 10:30:39 +0800

    移除过期插件jquery.turbolinks

--- app/assets/javascripts/frameworks.js
+++ app/assets/javascripts/frameworks.js
@@ -2,13 +2,12 @@
 //******** 由于依赖关系，在视图中需要先引入application.js再引入此文件***************
 //**************************************************************************
 //= require jquery
-//= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap
 //= require vue
-//= require turbolinks
 //= require go
 //= require jquery-zoom
 //= require bootstrap-datepicker
 //= require jquery-labelauty
 //= require swiper
+//= require turbolinks
